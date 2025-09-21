abc7da652be3683419d3d26a5904f715559aceef
emaiti-ui <castellanosvilla007@gmail.com>
2025-09-20 23:30:52 -0600

    nuevos scripts para DB2

--- eliminar1.m
+++ eliminar1.m
@@ -1,1 +1,12 @@
-%%
+%% FASE 1: LECTURA Y SEGMENTACIÓN
+%Lee imagen a color y obtiene sus dimensiones
+Im_color = imread(fullfile(ruta, archivos{i}));
+[alto, ancho, canales] = size(Im_color);
+
+
+%% FASE 2: CONVERSIÓN A ESCALA DE GRISES Y BINARIZACIÓN
+% Convierte a escala de grises para simplificar el procesamiento
+Ig = rgb2gray(I_segmentada);
+%figure(i + 2);
+%imshow(Ig);
+title('Imagen en Escala de Grises');
